528e70624cabd263328d300c131e897512fa1e8b
abhishekkkk-15 <mrabhi748@gmail.com>
2025-04-08 20:23:29 +0530

    backend changes

--- backend/index.js
+++ backend/index.js
@@ -6,8 +6,8 @@ import flowRoute from "./routes/emailRoutes.js";
 import userRoute from "./routes/userRoutes.js";
 import auth from "./utils/auth.js";
 import cookieParser from "cookie-parser";
-import jwt, { decode } from 'jsonwebtoken';
-import path from 'path'
+import jwt, { decode } from "jsonwebtoken";
+import path from "path";
 import { fileURLToPath } from "url";
 
 dotenv.config();
@@ -16,31 +16,36 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-app.use(cors({
-  origin: 'http://localhost:5173',
-  credentials: true 
-}))
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
-app.use((req,res,next)=>{
-  const token = req.cookies.token
-  if(!token){
-    next()
-    return
+app.use((req, res, next) => {
+  const token = req.cookies.token;
+  if (!token) {
+    next();
+    return;
   }
-    const decoded = jwt.verify(token, process.env.JWT_SECRET );
-      req.user = decoded; 
-  console.log("its comming here")
-  next()
-})
+  const decoded = jwt.verify(token, process.env.JWT_SECRET);
+  req.user = decoded;
+  console.log("its comming here");
+  next();
+});
 mongoose
   .connect(
     "mongodb+srv://mrabhi748:abhishek1122@cluster0.1s9sife.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
   )
   .then(() => console.log("MongoDB connected"));
-  app.use(express.static(path.join(__dirname, "dist")));
 app.use("/api/auth", userRoute);
 app.use("/api/flow", auth, flowRoute);
+app.use(express.static(path.join(__dirname, "dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
