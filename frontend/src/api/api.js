8ed517140d970e8669bfe8ff54fc8bfd2bf0e637
abhishekkkk-15 <mrabhi748@gmail.com>
2025-04-08 20:11:14 +0530

    rebuilding frontend

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = `${import.meta.env.VITE_API_URL}/api`;
-console.log( `${import.meta.env.VITE_API_URL}/api`)
+axios.defaults.baseURL = `${import.meta.env.VITE_API_URL}api`;
 axios.defaults.withCredentials = true;
 
 export const getLogedUserInfo = async () => await axios.get("/auth/userInfo");
