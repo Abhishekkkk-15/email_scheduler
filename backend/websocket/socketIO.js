4a2205af98c4c5a0ff7ae5c44e069529ec275a9d
abhishekkkk-15 <mrabhi748@gmail.com>
2026-01-12 23:22:15 +0530

    fix: backend websocket env fix

--- backend/websocket/socketIO.js
+++ backend/websocket/socketIO.js
@@ -1,7 +1,8 @@
 import { Server } from "socket.io";
 import http from "http";
 import express from "express";
-
+import { config } from "dotenv";
+config();
 const app = express();
 const server = http.createServer(app);
 
